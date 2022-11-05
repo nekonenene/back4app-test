c9efcede26d43b832f11883d642da19f134d9464
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-11-05 22:20:58 +0900

    Add spaces

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit
+          Edit&nbsp;
           <code>src/App.tsx</code>
-          and save to reload.
+          &nbsp;and save to reload.
         </p>
         <a
           className="App-link"
