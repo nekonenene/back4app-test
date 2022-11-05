e31e26ee184e82bd004eb3661a61eb6e44f879b7
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-11-05 22:01:44 +0900

    Add .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  extends: [
+    'airbnb',
+  ],
+};
